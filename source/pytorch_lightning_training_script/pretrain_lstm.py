1b6576825f808836ff042a0d93215ccee11f4e07
Kobayashi-Kei <ketakeketake@gmail.com>
2023-10-17 22:16:29 +0900

    実装ミスでPretrain_LSTMでLSTMをoptimizerに与えてなかったのを修正

--- source/pytorch_lightning_training_script/pretrain_lstm.py
+++ source/pytorch_lightning_training_script/pretrain_lstm.py
@@ -449,18 +449,23 @@ class Specter(pl.LightningModule):
 
     def configure_optimizers(self):
         """Prepare optimizer and schedule (linear warmup and decay)"""
-        model = self.bert
         no_decay = ["bias", "LayerNorm.weight"]
+
+        # Combine parameters of BERT and LSTM
+        model_parameters = list(self.bert.named_parameters()) + \
+            list(self.lstm.named_parameters())
+
         optimizer_grouped_parameters = [
             {
-                "params": [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)],
+                "params": [p for n, p in model_parameters if not any(nd in n for nd in no_decay)],
                 "weight_decay": self.hparams.weight_decay,
             },
             {
-                "params": [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)],
+                "params": [p for n, p in model_parameters if any(nd in n for nd in no_decay)],
                 "weight_decay": 0.0,
             },
         ]
+        
         if self.hparams.adafactor:
             optimizer = Adafactor(
                 optimizer_grouped_parameters, lr=self.hparams.lr, scale_parameter=False, relative_step=False
